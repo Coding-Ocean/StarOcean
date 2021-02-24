38558d14a0dd2c86e3d79b0aa47b80f706b34071
Coding-Ocean <eijo524@gmail.com>
2021-02-24 14:36:37 +0900

    recreate

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -20,13 +20,13 @@ void gmain() {
     struct POS {
         float x, y, z;
     };
-    const int num = 1000;
+    const int num = 300;
     struct POS p[num];
     //星をランダムに配置する
     for (int i = 0; i < num; i++) {
-        p[i].x = random(-0.9f, 0.9f);
+        p[i].x = random(-0.7f, 0.7f);
         p[i].y = random(-0.5f, 0.5f);
-        p[i].z = random(0.0f, 1.0f);
+        p[i].z = random(0.1f, 1.0f);
     }
     //ポイントor画像の表示用スイッチ
     int sw = 0;
@@ -34,46 +34,42 @@ void gmain() {
     ShowCursor(FALSE);
     //メインループ
     while (notQuit) {
-        //奥から手前に移動
-        for (int i = 0; i < num; i++) {
-            p[i].z += -0.008f;
-            if (
-                p[i].x < -1.1f ||
-                p[i].x > 1.1f ||
-                p[i].y < -0.6f ||
-                p[i].y > 0.6f ||
-                p[i].z <= 0.0f
-                ) {
-                p[i].x = random(-0.9f, 0.9f);
-                p[i].y = random(-0.5f, 0.5f);                p[i].z = 1.0f;
-            }
-        }
-        //２０％ぐらい透明の黒で塗りつぶす→軌跡を描く
+        //半透明の黒で塗りつぶす→軌跡を描く
         strokeWeight(0);
-        fill(0, 0, 0, 50);
+        fill(0, 0, 0, 90);
         rectMode(CORNER);
         rect(0, 0, width, height);
-        //ポイントor画像を描く
-        mathAxis(1,0,0);
-        strokeWeight(20);
+        //手前に移動後、ポイントor画像を描く
+        axisMode(NODRAW);
+        mathAxis(1.0f);
         if (isTrigger(KEY_Z))++sw %= 3;
+        rectMode(CENTER);
+        stroke(200, 255, 200);
         for (int i = 0; i < num; i++) {
-            float x = p[i].x;
-            float y = p[i].y;
-            float z = p[i].z;
-            float size = 1.0f - z;
-            strokeWeight(size * 20);
-            stroke(200, 255, 255);
-            if (sw==0) {
-                mathPoint(x / z, y / z);
+            //奥から手前に移動
+            p[i].z += -0.008f;
+            float x = p[i].x / p[i].z;
+            float y = p[i].y / p[i].z;
+            //ウィンドウから出たら奥に誕生させる
+            if (p[i].z <= 0.0f ||
+                x > 1.0f || x < -1.0f ||
+                y > 0.56f || y < -0.56f
+                ) {
+                x = p[i].x = random(-0.7f, 0.7f);
+                y = p[i].y = random(-0.5f, 0.5f);
+                p[i].z = 1.0f;
+            }
+            //点or画像の描画
+            float size = 1.0f - p[i].z;
+            if (sw == 0) {
+                strokeWeight(size * 20);
+                mathPoint(x, y);
             }
             else if (sw == 1) {
-                rectMode(CENTER);
-                mathImage(unkoImg, x / z, y / z, 0, size);
+                mathImage(ufoImg, x, y, 0, size);
             }
             else if (sw == 2) {
-                rectMode(CENTER);
-                mathImage(ufoImg, x / z, y / z, 0, size);
+                mathImage(unkoImg, x, y, 0, size);
             }
         }
     }
